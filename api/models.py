1392cfc6c183fdb8740b9f8658a499e5f64a89ed
Augusto Kestering Longo <augusto.kestering@gmail.com>
2022-11-01 00:01:54 -0300

    Add get_repository task

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class GitHubUser(models.Model):
     login = models.CharField(max_length=255)
-    github_id = models.IntegerField()
+    github_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=255)
     blog = models.URLField()
     public_repos = models.IntegerField()
@@ -14,12 +14,8 @@ class GitHubUser(models.Model):
 
 
 class UserRepository(models.Model):
+    name = models.CharField(max_length=255)
     github_user = models.ForeignKey(GitHubUser, on_delete=models.SET_NULL, null=True)
-    github_id = models.IntegerField()
+    github_id = models.IntegerField(unique=True)
     private = models.BooleanField()
     description = models.TextField()
-    forks = models.IntegerField()
-    open_issues_count = models.IntegerField()
-    stargazers_count = models.IntegerField()
-    watchers_count = models.IntegerField()
-    language = models.CharField(max_length=100)
--- api/tasks.py
+++ api/tasks.py
@@ -1,1 +1,34 @@
+import re
+
+import requests
 from celery import shared_task
+
+from .models import GitHubUser, UserRepository
+
+
+@shared_task(ignore_result=True, acks_late=True)
+def get_repository():
+    endpoint = "https://api.github.com/repositories"
+    last_repo = UserRepository.objects.last()
+
+    if last_repo:
+        endpoint = f"{endpoint}?since={last_repo.github_id + 1}"
+
+    r = requests.get(endpoint)
+
+    search_pattern = "\<.*?\>"
+    re.search(search_pattern, r.headers.get("link"))[0]
+
+    for repository in r.json():
+        owner = repository["owner"]
+        try:
+            github_user = GitHubUser.objects.get(github_id=owner["id"])
+        except GitHubUser.DoesNotExist:
+            print(owner)
+
+        UserRepository.objects.create(
+            github_id=repository["id"],
+            private=repository["private"],
+            description=repository["description"],
+            name=repository["name"],
+        )

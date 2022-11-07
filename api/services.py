d76565bb7d33d4518dec69ec5c0e51840749502a
Augusto Kestering Longo <augusto.kestering@gmail.com>
2022-11-07 20:33:16 -0300

    Change create user service

--- api/services.py
+++ api/services.py
@@ -9,15 +9,15 @@ def create_github_user(username: str) -> GitHubUser:
     r = requests.get(endpoint).json()
 
     github_user = GitHubUser.objects.create(
-        login=r["login"],
+        login=r.get("login"),
         github_id=r["id"],
-        name=r["name"],
-        blog=r["blog"],
-        public_repos=int(r["public_repos"]),
-        public_gists=int(r["public_gists"]),
-        followers=int(r["followers"]),
-        following=int(r["following"]),
-        bio=r["bio"],
+        name=r.get("name"),
+        blog=r.get("blog"),
+        public_repos=int(r.get("public_repos")),
+        public_gists=int(r.get("public_gists")),
+        followers=int(r.get("followers")),
+        following=int(r.get("following")),
+        bio=r.get("bio"),
     )
 
     return github_user
